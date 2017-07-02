7583ddc8d3ca4bc30104bfe741a7f2444d140989
eti0 <eti@eti.tf>
2017-07-02 18:03:10 +0200

    add $color values to script/barinfo.sh

--- scripts/barinfo.sh
+++ scripts/barinfo.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 # requires https://github.com/stark/siji installed to display icons
 
+# define the colors
+accent="%{F#B6B6B6}"
+text="%{F#eee}"
+
 clock() {
 	datetime=$(date +%R)
-	echo -n %{F#B6B6B6}%{F#eee} $datetime
+	echo -n $accent$text $datetime
 }
 
 sound() {
 	level=$(amixer get Master 2>&1 | awk '/Front Left:/{gsub(/[\[\]]/, "", $5); print $5}')
-	echo %{F#B6B6B6}%{F#eee} $level
+	echo $accent$text $level
 }
 
 song() {
@@ -16,7 +20,7 @@ song() {
 	playing=$(mpc -p 7755 status | grep -o 'playing')
 
 	if test "$playing" = "playing"; then
-		echo %{F#B6B6B6}%{F#eee} $csong
+		echo $accent$text $csong
 	else test "$playing" = "";
 		echo ''
 	fi
@@ -26,44 +30,44 @@ desktops() {
 	cur=`xprop -root _NET_CURRENT_DESKTOP | awk '{print $3}'`
 	tot=`xprop -root _NET_NUMBER_OF_DESKTOPS | awk '{print $3}'`
 	for w in `seq 0 $((cur - 1))`; do line="${line} ◽"; done
-	line="${line} %{F#eee}◾%{F#B6B6B6}"
+	line="${line} $text◾$accent"
 	for w in `seq $((cur + 2)) $tot`; do line="${line} ◽"; done
 	echo $line
 }
 
 network() {
 	cnetwork=$(iwgetid -r)
-	echo %{F#B6B6B6}%{F#eee} $cnetwork
+	echo $accent$text $cnetwork
 }
 
 battery() {
     percent=$(cat /sys/class/power_supply/BAT0/capacity)
     power_now=$(cat /sys/class/power_supply/BAT0/power_now)
 
-    fullthing="${symbol}%{F#eee} ${percent}%"
+    fullthing="${symbol}$text ${percent}%"
 
     if test $power_now = "0"; then
         symbol=""
-        echo -n "${symbol}%{F#eee} ${percent}%"
+        echo -n "${symbol}$text ${percent}%"
     elif test $percent = "98"; then
         symbol=""
-        echo -n "${symbol}%{F#eee} 100%"
+        echo -n "${symbol}$text 100%"
     else
         if test $percent -gt 60; then
             symbol=""
-            echo -n "${symbol}%{F#eee} ${percent}%"
+            echo -n "${symbol}$text ${percent}%"
         elif test $percent -gt 30; then
             symbol=""
-            echo -n "${symbol}%{F#eee} ${percent}%"
+            echo -n "${symbol}$text ${percent}%"
         else
             symbol=""
-            echo -n "${symbol}%{F#eee} ${percent}%"
+            echo -n "${symbol}$text ${percent}%"
         fi
     fi
 }
 
 # print all
 while true; do
-	echo "%{F#B6B6B6}%{l} $(desktops) %{c}$(clock) %{r}$(network) $(sound) %{F#B6B6B6}$(battery) "
+	echo "$accent%{l} $(desktops) %{c}$(clock) %{r}$(network)  $(sound)  $accent$(battery) "
 	sleep 0.2
 done
