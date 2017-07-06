695ffc816d0af7571cedfa8749e0f5b4e5071d1c
eti0 <eti@eti.tf>
2017-07-06 16:22:05 +0200

    modified scripts/barinfo.sh

--- scripts/barinfo.sh
+++ scripts/barinfo.sh
@@ -40,9 +40,10 @@ rsong() {
 desktops() {
 	cur=`xprop -root _NET_CURRENT_DESKTOP | awk '{print $3}'`
 	tot=`xprop -root _NET_NUMBER_OF_DESKTOPS | awk '{print $3}'`
-	for w in `seq 0 $((cur - 1))`; do line="${line}$accent◽ "; done
-	line="${line}$text◾"
-	for w in `seq $((cur + 2)) $tot`; do line="${line}$accent ◽"; done
+
+	for w in `seq 0 $((cur - 1))`; do line="${line}$text◽ "; done
+	line="${line}$accent◾"
+	for w in `seq $((cur + 2)) $tot`; do line="${line}$text ◽"; done
 	echo $line
 }
 
