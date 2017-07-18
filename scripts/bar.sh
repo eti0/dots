554f2fd75b8651886be66c8713cfcf58883c4d08
eti0 <eti@eti.tf>
2017-07-18 17:48:14 +0100

    change font declaration in scripts/bar.sh

--- scripts/bar.sh
+++ scripts/bar.sh
@@ -5,8 +5,8 @@ user=$(whoami)
 source "/home/$user/.scripts/colors.sh"
 
 ~/.scripts/barinfo.sh | lemonbar \
-    -f '-benis-lemon-medium-r-normal--10-110-75-75-m-50-iso10646-1' \
-    -f '-wuncon-siji-medium-r-normal--10-100-75-75-c-80-iso10646-1' \
+    -f '-benis-lemon-*' \
+    -f '-wuncon-siji-*' \
     -g "x30" \
     -B "$bgn" \
     | sh
