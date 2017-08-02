27046dae38576aa50e94188cd9c5a8398406e14f
eti0 <eti@eti.tf>
2017-08-02 17:21:05 +0100

    added album art script

---
+++ scripts/artwork/art.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+# vars
+csf=`mpc -f %file% | head -1`
+csil="/tmp/mpdimg.png"
+csbil="/usr/scripts/artwork/bg.png"
+
+
+# exec
+ffmpeg -loglevel 0 -y -i "/home/eti/Music/$csf" -vf scale=-1:200 "$csil"
+
+n30f -x "10" -y "40" -c "killall n30f" "$csbil" &
+n30f -x "12" -y "48" -c "killall n30f" "$csil"
