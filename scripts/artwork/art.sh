088a93ddea5e8d5af1f3787e2739eada43da2e86
eti0 <eti@eti.tf>
2017-08-03 11:50:11 +0100

    updated scripts/artwork

--- scripts/artwork/art.sh
+++ scripts/artwork/art.sh
@@ -5,12 +5,19 @@
 csf=`mpc -f %file% | head -1`
 csil="/tmp/mpdimg.png"
 csbil="/usr/scripts/artwork/bg.png"
+pointeril="/usr/scripts/artwork/pointer.png"
 
-
-# exec
+# extract the album art
 ffmpeg -loglevel 0 -y -i "/home/eti/Music/$csf" -vf scale=-1:200 "$csil"
 
-n30f -x "10" -y "40" -c "killall n30f" "$csbil" &
-n30f -x "12" -y "48" -c "killall n30f" "$csil"
+# pointer
+n30f -x "67" -y "34" -c "killall n30f" "$pointeril" &
 
+# background
+n30f -x "10" -y "40" -c "killall n30f" "$csbil" &
+
+# album art
+n30f -x "12" -y "42" -c "killall n30f" "$csil"
+
+# delete the album art
 rm "$csil"
