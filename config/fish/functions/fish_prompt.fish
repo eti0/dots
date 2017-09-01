a0f33c1f2285b349b3c00037917dfca1c1f2fee9
eti0 <eti@eti.tf>
2017-09-01 02:44:49 +0200

    updated fish_prompt.fish

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -8,50 +8,47 @@
 # - Git branch and dirty state (if inside a git repo)
 
 function _git_branch_name
-    echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
 end
 
 function _git_is_dirty
-    echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
 end
 
 function fish_prompt
-    set -l last_status $status
+  set fish_greeting ""
 
-    set -l cyan (set_color cyan)
-    set -l yellow (set_color yellow)
-    set -l red (set_color red)
-    set -l blue (set_color blue)
-    set -l green (set_color green)
-    set -l normal (set_color normal)
+  set -l last_status $status
 
-    set -l cwd $green(basename (pwd | sed "s:^$HOME:~:"))
+  set -l cyan (set_color cyan)
+  set -l yellow (set_color yellow)
+  set -l red (set_color red)
+  set -l blue (set_color blue)
+  set -l green (set_color green)
+  set -l normal (set_color normal)
 
-    # Display [venvname] if in a virtualenv
-    if set -q VIRTUAL_ENV
-        echo -n -s (set_color -b cyan black) '[' (basename "$VIRTUAL_ENV") ']' $normal ' '
+  set -l cwd $green(basename (pwd | sed "s:^$HOME:~:"))
+  
+ # Print pwd or full path
+  echo -n -s $cwd $normal
+
+  # Show git branch and status
+  if [ (_git_branch_name) ]
+    set -l git_branch (_git_branch_name)
+
+    if [ (_git_is_dirty) ]
+      set git_info $red $git_branch " ×" $normal
+    else
+      set git_info $green $git_branch $normal
     end
+    echo -n -s ' : ' $git_info $normal
+  end
 
-    # Print pwd or full path
-    echo -n -s $cwd $normal
+  set -l prompt_color $red
+  if test $last_status = 0
+    set prompt_color $normal
+  end
 
-    # Show git branch and status
-    if [ (_git_branch_name) ]
-        set -l git_branch (_git_branch_name)
-
-        if [ (_git_is_dirty) ]
-            set git_info $red $git_branch " ×" $normal
-        else
-            set git_info $normal $git_branch $normal
-        end
-        echo -n -s ' : ' $git_info $normal
-    end
-
-    set -l prompt_color $red
-    if test $last_status = 0
-        set prompt_color $normal
-    end
-
-    # Terminate with a nice prompt char
-    echo -e -n -s $prompt_color ' — ' $normal
+  # Terminate with a nice prompt char
+  echo -e -n -s $prompt_color ' — ' $normal
 end
