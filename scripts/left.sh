a53f2ed2170551467fb30d8031e7bd7ccfbf4e4a
eti <eti@eti.tf>
2017-09-04 22:12:34 +0200

    fix symlink of scripts/left.sh

--- scripts/left.sh
+++ scripts/left.sh
@@ -1,1 +1,13 @@
-/usr/scripts/left.sh
+#!/usr/bin/env bash
+
+devices="$(xsetwacom --list devices | grep -o "Wacom" | head -n1)"
+
+if [ "$devices" == "Wacom" ] ; then
+	xsetwacom set "Wacom Bamboo Connect Pen stylus" Rotate half
+	xsetwacom set "Wacom Bamboo Connect Pen eraser" Rotate half
+	printf "done! \n"
+	exit 0
+else
+	printf "no matching devices found \n"
+	exit 1
+fi
