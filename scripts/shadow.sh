bbabbe716a7b1261a8a42f72726d96c84896ef7a
eti <eti@eti.tf>
2017-09-04 22:13:51 +0200

    added scrips/shadow.sh

---
+++ scripts/shadow.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+name="$(basename $1)"
+
+echo "$name"
+
+convert "$1"  \( +clone -background "black" -shadow "5%5x3+0+2" \) +swap -background "transparent" -layers merge +repage "/home/eti/Pictures/Screenshots/shadow/shadow_$name"
