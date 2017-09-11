fd8b75a22bf045d7114e7a82cdceb490231d9f15
eti0 <eti@eti.tf>
2017-09-11 11:22:17 +0200

    updated Xdefaults & fish_prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,54 +1,23 @@
-# name: gnuykeaj
-# ---------------
-# Based on clearance, which is based off idan. 
-# 1 line minimal, beautiful version of clearance.
-# Display the following bits on the left:
-# - Virtualenv name (if applicable, see https://github.com/adambrenecki/virtualfish)
-# - Current directory name
-# - Git branch and dirty state (if inside a git repo)
-
-function _git_branch_name
-  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
-end
-
-function _git_is_dirty
-  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
-end
-
 function fish_prompt
-  set fish_greeting ""
+    set -l last_status $status
 
-  set -l last_status $status
+    set -l cyan (set_color cyan)
+    set -l yellow (set_color yellow)
+    set -l red (set_color red)
+    set -l blue (set_color blue)
+    set -l green (set_color green)
+    set -l normal (set_color normal)
 
-  set -l cyan (set_color cyan)
-  set -l yellow (set_color yellow)
-  set -l red (set_color red)
-  set -l blue (set_color blue)
-  set -l green (set_color green)
-  set -l normal (set_color normal)
+    set -l cwd $red(basename (pwd | sed "s:^$HOME:~:"))
 
-  set -l cwd $green(basename (pwd | sed "s:^$HOME:~:"))
-  
- # Print pwd or full path
-  echo -n -s $cwd $normal
+    # Print pwd or full path
+    echo -n -s $cwd $normal
 
-  # Show git branch and status
-  if [ (_git_branch_name) ]
-    set -l git_branch (_git_branch_name)
-
-    if [ (_git_is_dirty) ]
-      set git_info $red $git_branch " ×" $normal
-    else
-      set git_info $green $git_branch $normal
+    set -l prompt_color $red
+    if test $last_status = 0
+        set prompt_color $normal
     end
-    echo -n -s ' : ' $git_info $normal
-  end
 
-  set -l prompt_color $red
-  if test $last_status = 0
-    set prompt_color $normal
-  end
-
-  # Terminate with a nice prompt char
-  echo -e -n -s $prompt_color ' — ' $normal
+    # Terminate with a nice prompt char
+    echo -e -n -s $prompt_color ' - ' $normal
 end
