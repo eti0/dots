1322897216682a6a607086b2f1a1cd91cfa6202c
gurtej <31964022+vsrch@users.noreply.github.com>
2017-09-16 18:12:57 +0530

    Improved nrnl.sh
    - Implemented the script in ```sh``` instead of ```fish``` for better portability.
    - Added an algorithm that allows easy formatting in ANSI escape codes.
      - ANSI escape codes have been related to base 16 colors.
      - Usage: Wrapping text in ```$f1``` and ```$rst``` will change foreground color to red.
    - No assumptions are made for *os*, *wm* and *init* variables. They are detected using ```coreutils``` and ```xprop```.
    - Output using *here document* (```cat <<<EOF```).
    
    Tell me if it works or not.
    I really like your concepts. Pardon me for stealing almost everything from you for my ricing.
    Thanks.

--- scripts/nrnl.sh
+++ scripts/nrnl.sh
@@ -1,30 +1,57 @@
-#!/usr/bin/env fish
-# no rice no life - display stuff that nobody cares about
+#!/usr/bin/env sh
+#
+# nrnl.sh - displays sys info
 
+# colors (algorithm by lolilolicon)
+f=3 b=4
+for j in f b; do
+	for i in {0..7}; do
+		printf -v $j$i "%b" "\e[${!j}${i}m"
+	done
+done
+bld=$'\e[1m'
+rst=$'\e[0m'
+inv=$'\e[7m'
 
-# set the colors - change this to your liking
-set fg (set_color normal)
-set c1 (set_color red)
-set c2 (set_color white)
+# detect
+user=$(whoami)
+host=$(hostname)
+kernel=$(uname -r)
+shell=$(basename $SHELL)
+os() {
+	os=$(find /etc -mindepth 1 -maxdepth 1 ! -type l | grep release)
+	os=${os##*/}
+	os=${os%-*}
+	export os
+}
+wm() {
+	id=$(xprop -root -notype _NET_SUPPORTING_WM_CHECK)
+	id=${id##* }
+	wm=$(xprop -id "$id" -notype -len 100 -f _NET_WM_NAME 8t)
+	wm=${wm/*_NET_WM_NAME = }
+	wm=${wm/\"}
+	wm=${wm/\"*}
+	wm=${wm,,} # use this to output in lowercase
+	export wm
+}
+init() {
+	init=$(readlink /sbin/init)
+	init=${init##*/}
+	export init
+}
 
+# exec
+os
+wm
+init
+cat <<EOF
 
-# set the variables
-set user (whoami)
-set host (hostname)
-set userinf "$user"$c1"@"$fg"$host"
-set kernel (uname -r)
-set shell (basename $SHELL)
-set os "void"
-set wm "openbox"
-set init "runit"
+$user$f2@$rst$host
 
+         ${f2}os${rst}:          ${os}
+┌───┐    ${f2}kernel${rst}:      ${kernel}
+│${f2}•˩•${rst}│    ${f2}shell${rst}:       $shell
+└───┘    ${f2}init${rst}:        $init
+         ${f2}wm${rst}:          $wm
 
-# execute everything
-echo
-echo $userinf
-echo $c1"	os"$fg":        $os"
-echo "┌───┐ "$c1"	kernel"$fg":    $kernel"
-echo "│"$c1"•˩•"$fg"│"$c1"	shell"$fg":     $shell"
-echo "└───┘"$c1"	init"$fg":      $init"
-echo $c1"	wm"$fg":        $wm"
-echo
+EOF
