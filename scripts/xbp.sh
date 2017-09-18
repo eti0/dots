9a3dfe51c3d5d5fdc33fce245fc8b8ed8023e9c7
eti0 <eti@eti.tf>
2017-09-18 23:35:46 +0200

    updated scripts/xbp.sh

--- scripts/xbp.sh
+++ scripts/xbp.sh
@@ -3,10 +3,13 @@
 
 if [ "$1" = "i" ] ; then
 	sudo xbps-install "${@:2}"
+	printf "\e[8;40;90t"
 elif [ "$1" = "s" ] ; then
 	sudo xbps-query -Rs "${@:2}"
+	printf "\e[8;40;90t"
 elif [ "$1" = "r" ] ; then
 	sudo xbps-remove "${@:2}"
+	printf "\e[8;40;90t"
 else
 	echo "no arguments given"
 	exit 1
