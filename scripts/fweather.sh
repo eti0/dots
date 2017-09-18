6500b5136e6ac51d0e78df2d074ffd59e3e7c328
eti0 <eti@eti.tf>
2017-09-19 01:01:16 +0200

    updated scripts/fweather.sh

--- scripts/fweather.sh
+++ scripts/fweather.sh
@@ -3,6 +3,7 @@
 # vars
 tmpf="/tmp/fweather"
 station="LFBD"
+ctime=$(date "+%H")
 temp=$(weather --no-cache -qm --headers="Temperature" "$station" | sed "s/Temperature: //" | sed "s/ //" | sed "s/C/°C/")
 
 
@@ -21,8 +22,34 @@ wcond() {
 	fi
 }
 
+icon() {
+	weather=$(weather --no-cache -qm --headers="Weather" "$station" | sed "s/Weather: //")
+	
+	if [[ "$ctime" -le "07" || "$ctime" -ge "20" ]] ; then
+	 	ico=" "
+		echo "$ico"
+	elif [ "$weather" == "light rain" ] ; then
+		ico=""
+		echo "$ico "
+	elif [ "$weather" == "showers in the vicinity" ] ; then
+		ico=""
+		echo "$ico "
+	elif [[ "$(wcond)" == "mostly cloudy" || "$(wcond)" == "mostly clear"  ]] ; then
+		ico=""
+		echo "$ico "
+	elif [ "$(wcond)" == "overcast" ] ; then
+		ico=""
+		echo "$ico "
+	elif [ "$(wcond)" == "clear" ] ; then
+		ico=""
+		echo "$ico "
+	else
+		:
+	fi
+}
+
 while true ; do
-	echo "$temp - $(wcond)" > "$tmpf"
+	echo "$(icon)$temp - $(wcond)" > "$tmpf"
 	cat "$tmpf"
 	sleep "15m"
 done
