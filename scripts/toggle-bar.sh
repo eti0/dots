fb922f7226d3423446ba12cfa4bd0104bda7b602
eti0 <eti@eti.tf>
2017-09-29 12:29:20 +0200

    updated scripts/toggle-bar.sh

--- scripts/toggle-bar.sh
+++ scripts/toggle-bar.sh
@@ -14,7 +14,7 @@ if [ $pid ]; then
 else
 	sed -i "s/<top>30<\/top>/<top>60<\/top>/g" "$HOME/.config/openbox/rc.xml"
 	openbox --reconfigure
-	convert -size "$monwx30" "xc:$background" "$tmpimg"
+	convert -size "$monw x30" "xc:$background" "$tmpimg"
 	n30f "$tmpimg" &
 	/usr/scripts/bar.sh &
 	sleep ".5s" && kill -9 "$(pidof n30f)"
