cad87e344ee4344fd6860fd9af8b242ecf74222e
eti0 <eti@eti.tf>
2017-09-30 14:23:32 +0200

    updated scripts/toggle-bar.sh

--- scripts/toggle-bar.sh
+++ scripts/toggle-bar.sh
@@ -11,23 +11,23 @@ pid=$(pidof "lemonbar")
 tmpimg="/tmp/toggle.png"
 
 # get the screen size
-monw=$(xdotool "getdisplaygeometry" | awk '{print $1;}')
+# monw=$(xdotool "getdisplaygeometry" | awk '{print $1;}')
 
 if [ $pid ] ; then
-	sed -i "s/<top>60<\/top>/<top>30<\/top>/g" "$HOME/.config/openbox/rc.xml"
-	sed -i "s/YPOS=\"60\"/YPOS=\"30\"/" "/usr/scripts/vol.sh"
+	sed -i "s/<top>70<\/top>/<top>30<\/top>/g" "$HOME/.config/openbox/rc.xml"
+	sed -i "s/YPOS=\"70\"/YPOS=\"30\"/" "/usr/scripts/vol.sh"
 	openbox --reconfigure
 	kill -9 "$pid"
 else
-	sed -i "s/<top>30<\/top>/<top>60<\/top>/g" "$HOME/.config/openbox/rc.xml"
-	sed -i "s/YPOS=\"30\"/YPOS=\"60\"/" "/usr/scripts/vol.sh"
+	sed -i "s/<top>30<\/top>/<top>70<\/top>/g" "$HOME/.config/openbox/rc.xml"
+	sed -i "s/YPOS=\"30\"/YPOS=\"70\"/" "/usr/scripts/vol.sh"
 	openbox --reconfigure
 
 	# since lemonbar is ugly on startup (empty spaces)
 	# i use n30f to display a png until the bar is done starting
-	convert -size "$monw x30" "xc:$background" "$tmpimg"
-	n30f "$tmpimg" &
+	convert -size "512x30" "xc:$background" "$tmpimg"
+	n30f "$tmpimg" -x "704" -y "20" &
 
-	/usr/scripts/bar.sh d &
-	sleep ".5s" && kill -9 "$(pidof n30f)" &
+	/usr/scripts/smpl.sh d &
+	sleep ".3s" && kill -9 "$(pidof n30f)" &
 fi
