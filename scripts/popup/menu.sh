b50eed084c6771e4f13849656f373912827afb85
eti0 <eti@eti.tf>
2017-10-22 13:27:11 +0200

    updated popup/menu.sh & themes/c

--- scripts/popup/menu.sh
+++ scripts/popup/menu.sh
@@ -6,21 +6,31 @@ set -e
 # vars
 pointer="/usr/scripts/popup/img/pointer.png"
 menu="/usr/scripts/popup/img/menu.png"
+width=$(xdotool "getdisplaygeometry" | awk '{print $1;}')
 height=$(xdotool "getdisplaygeometry" | awk '{print $2;}')
 ypos=$(expr "$height" - "242")
 item="/usr/scripts/popup/img/menu-items/"
 
 
 # exec
+# gen null
+convert -size "$width x $height" "xc:transparent" "/tmp/null.png"
+
+# kill
+n30f -c "killall n30f" "/tmp/null.png" &
+
+sleep ".05s"
+
 # background & pointer
 n30f -t "background" -x "14" -y "$(expr "$height" - "184")" -c "killall n30f" "$menu" &
 n30f -t "pointer" -x "33" -y "$(expr "$ypos" + "204")" -c "killall n30f" "$pointer" &
 
 sleep ".05s"
 
-n30f -t "menu" -x "34" -y "$(expr "$height" - "68")" -c "chromium & killall n30f" "$item/web.png" &
-n30f -t "menu" -x "34" -y "$(expr "$height" - "88")" -c "cd $HOME && urxvtc & killall n30f" "$item/term.png" &
-n30f -t "menu" -x "34" -y "$(expr "$height" - "108")" -c "pcmanfm -n $HOME & killall n30f" "$item/files.png" &
-n30f -t "menu" -x "34" -y "$(expr "$height" - "128")" -c "obconf & killall n30f" "$item/obconf.png" &
-n30f -t "menu" -x "34" -y "$(expr "$height" - "148")" -c "feh --randomize --bg-fill $HOME/ownCloud/Wallpapers & killall n30f" "$item/random.png" &
-n30f -t "menu" -x "34" -y "$(expr "$height" - "168")" -c "pavucontrol & killall n30f" "$item/pavucontrol.png" &
+# items
+n30f -t "items" -x "34" -y "$(expr "$height" - "68")" -c "chromium & killall n30f" "$item/web.png" &
+n30f -t "items" -x "34" -y "$(expr "$height" - "88")" -c "cd $HOME && urxvtc & killall n30f" "$item/term.png" &
+n30f -t "items" -x "34" -y "$(expr "$height" - "108")" -c "pcmanfm -n $HOME & killall n30f" "$item/files.png" &
+n30f -t "items" -x "34" -y "$(expr "$height" - "128")" -c "obconf & killall n30f" "$item/obconf.png" &
+n30f -t "items" -x "34" -y "$(expr "$height" - "148")" -c "feh --randomize --bg-fill $HOME/ownCloud/Wallpapers & killall n30f" "$item/random.png" &
+n30f -t "items" -x "34" -y "$(expr "$height" - "168")" -c "pavucontrol & killall n30f" "$item/pavucontrol.png" &
