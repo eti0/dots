dcce4497eef6baf177ef4df6f6c8d5a9054e3ebc
eti0 <eti@eti.tf>
2017-10-29 02:14:43 +0200

    rose -> green | updated Xdefaults, scripts/colors.sh, themes/c

--- scripts/colors.sh
+++ scripts/colors.sh
@@ -13,17 +13,19 @@ txt="%{F$text}"
 wrn="%{F$warning}"
 
 # accents
-acc0="#f8818e"
-acc1="#f88b98"
-acc2="#f995a1"
-acc3="#f99fa9"
-acc4="#faa8b2"
+acc0="#66cc88"
+
+# acc1="#f88b98"
+# acc2="#f995a1"
+# acc3="#f99fa9"
+# acc4="#faa8b2"
 
 a0="%{B$acc0}"
-a1="%{B$acc1}"
-a2="%{B$acc2}"
-a3="%{B$acc3}"
-a4="%{B$acc4}"
+
+# a1="%{B$acc1}"
+# a2="%{B$acc2}"
+# a3="%{B$acc3}"
+# a4="%{B$acc4}"
 
 af0="%{F$acc0}"
 af1="%{F$acc1}"
