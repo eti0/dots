a0614e345ae2b869f8fef817ee529a783214760c
eti0 <eti@eti.tf>
2017-11-22 11:53:09 +0100

    updated Xdefaults, fish_prompt.fish

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -10,7 +10,7 @@ function fish_prompt
     set -l white (set_color white)
     set -l normal (set_color normal)
 
-    set -l cwd $red(basename (pwd | sed "s:^$HOME:~:"))
+    set -l cwd $green(basename (pwd | sed "s:^$HOME:~:"))
 
     # Print pwd or full path
     echo -n -s $cwd $normal
