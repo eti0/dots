35d7cd0acc42d159699636a975264c89e8a44740
eti0 <eti@eti.tf>
2018-01-09 01:25:14 +0100

    updated config/compton.conf, scripts/xbp.sh, themes/c

--- scripts/xbp.sh
+++ scripts/xbp.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 # xbps shortcuts
 
-clean() {
-	printf "\e[8;40;90t"
-	/usr/scripts/center.sh
-}
-
 if [ "$1" = "i" ] ; then
 	sudo xbps-install "${@:2}"
 elif [ "$1" = "s" ] ; then
