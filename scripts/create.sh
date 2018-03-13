e0f464a4ea60a75cdc3e7043cb85d82e7ae6376d
eti0 <eti@eti.tf>
2018-03-13 22:17:02 +0100

    added config/fontconfig, updated Xdefaults, scripts/create.sh

--- scripts/create.sh
+++ scripts/create.sh
@@ -2,20 +2,14 @@
 # create an urxvt window
 
 # vars
-tmp="/tmp/sel"
 m="10"
 wd="6"
-hd="19"
+hd="15"
 
 # exec
-slop -nl -b "5" -c "1 1 1 0" > "$tmp"
-sed -i 's/x/ /; s/+/ /; s/+/ /' "$tmp"
+read "w" "h" "x" "y" < <(slop -nl -b "5" -c "1 1 1 0" -f "%w %h %x %y")
 
-w="$(cat $tmp | awk '{print $1}')"
 w="$(printf $(( ("$w" + "$m") / "$wd" )) )"
-h="$(cat $tmp | awk '{print $2}')"
 h="$(printf $(( ("$h" + "$m") / "$hd" )) )"
-x="$(cat $tmp | awk '{print $3}')"
-y="$(cat $tmp | awk '{print $4}')"
 
 urxvt -g "$w"x"$h"+"$x"+"$y"
