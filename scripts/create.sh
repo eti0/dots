74dd205d9b5f7ef88dec6eeba9ba4e1743c347ba
eti0 <eti@eti.tf>
2018-03-13 23:01:43 +0100

    updated scripts/create.sh

--- scripts/create.sh
+++ scripts/create.sh
@@ -2,14 +2,13 @@
 # create an urxvt window
 
 # vars
-m="10"
 wd="6"
 hd="15"
 
 # exec
-read "w" "h" "x" "y" < <(slop -nl -b "5" -c "1 1 1 0" -f "%w %h %x %y")
+read "w" "h" "x" "y" < <(slop -l -b "5" -c "1 1 1 0" -f "%w %h %x %y")
 
-w="$(printf $(( ("$w" + "$m") / "$wd" )) )"
-h="$(printf $(( ("$h" + "$m") / "$hd" )) )"
+w="$(printf $(( "$w" / "$wd" )) )"
+h="$(printf $(( "$h" / "$hd" )) )"
 
 urxvt -g "$w"x"$h"+"$x"+"$y"
