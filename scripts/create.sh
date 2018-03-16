83c86afa13316e5b7a9372f77c2033a49ede598f
eti0 <eti@eti.tf>
2018-03-16 17:35:11 +0100

    updated config/openbox & scripts/create.sh

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
-# create an urxvt window
-
-# vars
-wd="6"
-hd="15"
+# create an urxvt window plan9-style
+# requires slop and xdotool
 
 # exec
 read "w" "h" "x" "y" < <(slop -l -b "5" -c "1 1 1 0" -f "%w %h %x %y")
 
-w="$(printf $(( "$w" / "$wd" )) )"
-h="$(printf $(( "$h" / "$hd" )) )"
-
-urxvt -g "$w"x"$h"+"$x"+"$y"
+urxvt -g +"$x"+"$y" &
+sleep ".1"
+xdotool windowsize "$(xdotool "getactivewindow")" "$w" "$h"
