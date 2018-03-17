f77bb52d4ef5f52978befa72a4e1fde8f58f9c65
eti0 <eti@eti.tf>
2018-03-17 13:29:14 +0100

    added scripts/layout.sh, updated Xdefaults, config/openbox, scripts/create.sh, themes/c

---
+++ scripts/layout.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# change the keyboard layout
+
+
+# vars
+file="/tmp/klayout"
+current="$(cat "$file")"
+lay1="us"
+lay2="fr"
+lay3="ru"
+
+
+# exec
+if [ -f "$file" ] ; then
+	if [ "$current" = "$lay1" ] ; then
+		setxkbmap "$lay2"
+		printf "$lay2" > "$file"
+		notify-send "keyboard layout: $lay2"
+	elif [ "$current" = "$lay2" ] ; then
+		setxkbmap "$lay3"
+		printf "$lay3" > "$file"
+		notify-send "keyboard layout: $lay3"
+	elif [ "$current" = "$lay3" ] ; then
+		setxkbmap "$lay1"
+		printf "$lay1" > "$file"
+		notify-send "keyboard layout: $lay1"
+	else
+		notify-send "???"
+	fi
+else
+	notify-send "$file not found. defaulting to $lay1 layout."
+	setxkbmap "$lay1"
+	printf "$lay1" > "$file"
+	sleep "1"
+	notify-send "keyboard layout: $current"
+fi
