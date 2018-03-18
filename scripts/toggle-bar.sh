f286cf40e674a28d40d4a5a0a0b340ef9041b108
eti0 <eti@eti.tf>
2018-03-18 17:03:22 +0100

    updated scripts/toggle-bar.sh

--- scripts/toggle-bar.sh
+++ scripts/toggle-bar.sh
@@ -18,39 +18,32 @@ tmpimg="/tmp/toggle.png"
 # execute
 if [ $pid ] ; then
 	# change openbox margins
-	# sed -i "s/<bottom>30<\/bottom>/<bottom>0<\/bottom>/g" "$HOME/.config/openbox/rc.xml"
-	# openbox --reconfigure
+	sed -i "s/<bottom>30<\/bottom>/<bottom>0<\/bottom>/g" "$HOME/.config/openbox/rc.xml"
+	openbox --reconfigure
 
 	# change dunst placement
-	# sed -i "s/-50\"/-20\"/g" "$HOME/.config/dunstrc"
-	# kill -9 "$(pidof dunst)"
-
-	# adapt center.sh
-	# sed -i "s/- 20/- 0/g" "/usr/scripts/center.sh"
+	sed -i "s/-50\"/-20\"/g" "$HOME/.config/dunstrc"
+	kill -9 "$(pidof dunst)"
 
 	# kill the bar
 	kill -9 "$pid"
 else
 	# change openbox margins
-	# sed -i "s/<bottom>0<\/bottom>/<bottom>30<\/bottom>/g" "$HOME/.config/openbox/rc.xml"
-	# openbox --reconfigure
+	sed -i "s/<bottom>0<\/bottom>/<bottom>30<\/bottom>/g" "$HOME/.config/openbox/rc.xml"
+	openbox --reconfigure
 
 	# change dunst placement
-	# sed -i "s/-20\"/-50\"/g" "$HOME/.config/dunstrc"
-	# kill -9 "$(pidof dunst)"
-
-	# adapt center.sh
-	# sed -i "s/- 0/- 20/g" "/usr/scripts/center.sh"
+	sed -i "s/-20\"/-50\"/g" "$HOME/.config/dunstrc"
+	kill -9 "$(pidof dunst)"
 
 	# since lemonbar is ugly on startup (empty spaces)
 	# i use n30f to display a png until the bar is done starting
-	# convert -size "$width x30" "xc:$background" "$tmpimg"
-	# n30f "$tmpimg" -y "$ypos" &
+	convert -size "$width x30" "xc:$background" "$tmpimg"
+	n30f "$tmpimg" -y "$ypos" &
 
 	# start the bar
-	# sleep ".5s" && /usr/scripts/wbr.sh l &
-	xv
+	sleep ".5s" && /usr/scripts/wbr.sh &
 
 	# kill n30f
-	# sleep "1s" && kill -9 "$(pidof n30f)" &
+	sleep "1s" && kill -9 "$(pidof n30f)" &
 fi
