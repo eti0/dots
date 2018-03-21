141b8f54fafe7eea9cf4471d4a4956fd759c8711
eti0 <eti@eti.tf>
2018-03-21 22:24:06 +0100

    updated scripts/wbr.sh

--- scripts/wbr.sh
+++ scripts/wbr.sh
@@ -1,175 +1,134 @@
 #!/usr/bin/env bash
-
-
-# fetch the colors
-source "/usr/scripts/colors.sh"
-
-
 # vars
-rrate=".2s"
-p="   "
-barh="30"
-margin="30"
+colors="/usr/scripts/colors.sh"
+refresh=".2"
+padding="   "
+height="30"
 font="-*-cure-*"
-iconfont="-*-siji-*"
+ifont="-*-siji-*"
+battery="BAT0"
+
+
+# colors
+source "$colors"
 
 
 # functions
-desktops() {
-	cur=`xprop -root _NET_CURRENT_DESKTOP | awk '{print $3}'`
-	tot=`xprop -root _NET_NUMBER_OF_DESKTOPS | awk '{print $3}'`
-
-	for w in `seq 0 $((cur - 1))`; do line="${line}- "; done
-	line="${line}÷"
-	for w in `seq $((cur + 2)) $tot`; do line="${line} -"; done
-	echo "$line"
+desktop() {
+	current="$(xdotool "get_desktop")"
+	
+	case "$current" in
+		"0")
+			desktop="un"
+			;;
+		"1")
+			desktop="deux"
+			;;
+		"2")
+			desktop="trois"
+			;;
+		"3")
+			desktop="quatre"
+			;;
+		"4")
+			desktop="cinq"
+			;;
+		*)
+			desktop="???"
+			;;
+	esac
+	
+	printf "$desktop"
 }
 
-workspace() {
-	cur=$(xdotool "get_desktop")
-
-	if [ "$cur" == "0" ] ; then
-		printf "un"
-	elif [ "$cur" == "1" ] ; then
-		printf "deux"
-	elif [ "$cur" == "2" ] ; then
-		printf "trois"
-	elif [ "$cur" == "3" ] ; then
-		printf "quatre"
-	elif [ "$cur" == "4" ] ; then
-		printf "cinq"
-	else
-		printf "???"
-	fi
-}
-
-song() {
-	csong=$(mpc current)
-	playing=$(mpc status | grep -o 'playing' )
-
-	if [ "$playing" == "playing" ]; then
-		echo "$p$csong$p"
-	else
-		:
-	fi
+mpd() {
+	current="$(mpc "current")"
+	
+	printf "$current"
 }
 
 spotify() {
 	current="$(sps "current")"
 	pid="$(pidof "spotify")"
+	
 	if [ "$pid" ] ; then
-		printf "$p$current$p"
+		printf "$padding$current$padding"
 	else
 		:
 	fi
 }
 
 window() {
-	cwindow=$(xdotool "getwindowfocus" "getwindowname")
-
-	if [ "$cwindow" == "Openbox" ] ; then
+	current="$(xdotool "getwindowfocus" "getwindowname")"
+	
+	if [ "$current" == "Openbox" ] ; then
 		:
 	else
-		echo "$p$cwindow$p"
+		printf "$current"
 	fi
-}
-
-crypto() {
-	ccv="$(cat "/tmp/crypto")"
-	echo "$ccv"
 }
 
 weather() {
 	cat "/tmp/weather"
 }
 
-network() {
-	cnetwork=$(iwgetid -r)
-
-	if [ "$cnetwork" == "" ]; then
-		echo "$wrn$txt"
-	else
-		echo "$cnetwork"
-	fi
-}
-
-sound() {
-	level=$(amixer get Master 2>&1 | awk '/Front Left:/{gsub(/[\[\]]/, "", $5); print $5}')
-	muted=$(amixer get Master | grep -o off | tail -n1)
-	
-	if [ "$muted" == "off" ]; then
-		echo "$wrn$txt"
-	else
-		echo "$level"
-	fi
-}
-
 battery() {
-    percent=$(cat /sys/class/power_supply/BAT0/capacity)
-    power=$(cat /sys/class/power_supply/BAT0/status)
-    
-	if [[ $power == "Charging" || $power == "Unknown" ]]; then
-		echo -n "$percent%"
-	else
-		if [ $percent -eq 100 ]; then
-			echo -n "$percent%"
-		elif [ $percent -gt 80 ] ; then
-			echo -n "$percent%"
-		elif [ $percent -gt 30 ]; then
-			echo -n "$percent%"
-		else 
-			echo -n "$percent%"
-		fi
-	fi
+	id="BAT0"
+	percent="$(cat "/sys/class/power_supply/"$id"/capacity")"
+	power="$(cat "/sys/class/power_supply/"$id"/status")"
+	
+	echo "$percent%%"
 }
 
 clock() {
-	datetime=$(date "+%a %R")
-	echo "$datetime"
+	clock="$(date "+%a %R")"
+	printf "$clock"
 }
 
-loop-desktop() {
+
+# loops
+dloop() {
 	while :; do
 		echo "%{l}\
-		$a1$af2%{A3:spotifycli --playpause &:}$p$(workspace)$p%{A}$txt$bg\
+		$a1$af2%{A3:sps 'play' &:}$padding$(desktop)$padding%{A}$txt$bg\
 		$a3$(spotify)$bg\
-		$a2$(window)$bg\
+		$a2$padding$(window)$padding$bg\
 		%{r}\
-		$a2%{A:notify-send 'updating the weather...' && weather &:}$p$(weather)$p%{A}$bg\
-		$a2%{A:calendar &:}$p$(clock)$p%{A}$bg"
-		sleep "$rrate"
+		$a2%{A:notify-send 'updating the weather' && weather &:}$padding$(weather)$padding%{A}$bg\
+		$a2%{A:calendar &:}$padding$(clock)$padding%{A}$bg"
+		sleep "$refresh"
 	done |\
-	
+
 	lemonbar \
-	    -f "$font" \
-   	    -f "$iconfont" \
-	    -g "x"$barh"" \
-	    | bash
+		-f "$font" \
+		-f "$ifont" \
+		-g "x"$height"" \
+	| bash
 }
 
-loop-laptop() {
+lloop() {
 	while :; do
 		echo "%{l}\
-		$a1$af2%{A3:spotifycli --playpause &:}$p$(workspace)$p%{A}$txt$bg\
+		$a1$af2%{A3:sps 'play' &:}$padding$(desktop)$padding%{A}$txt$bg\
 		$a3$(spotify)$bg\
-		$a2$(window)$bg\
+		$a2$padding$(window)$padding$bg\
 		%{r}\
-		$a2%{A:batstat &:}$p$(battery)$p%{A}$bg\
-		$a2%{A:calendar &:}$p$(clock)$p%{A}$bg"
-		sleep "$rrate"
+		$a2%{A:batstat &:}$padding $(battery)$padding%{A}$bg\
+		$a2%{A:calendar &:}$padding $(clock)$padding%{A}$bg"
+		sleep "$refresh"
 	done |\
-	
+
 	lemonbar \
-	    -f "$font" \
-	    -f "$iconfont" \
-	    -g "x"$barh"" \
-	    | bash
+		-f "$font" \
+		-f "$ifont" \
+		-g "x"$height"" \
+	| bash
 }
 
 
 # exec
-if [ -f "/sys/class/power_supply/BAT0/status" ] ; then
-	loop-laptop
+if [ -f "/sys/class/power_supply/"$battery"/status" ] ; then
+	lloop
 else
-	loop-desktop
+	dloop
 fi
