6ca63bd85bdb60a041d856bbc7f881f335533a9b
eti0 <eti@eti.tf>
2018-04-11 21:14:37 +0200

    added scripts/dict.sh, updated config/micro

---
+++ scripts/dict.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# vars
+www="http://www.dictionary.com/browse"
+word="$1"
+
+# funcs
+get() {
+	cmd=$(curl -s "$www/$word" | grep -oP '(?<=<meta name="description" content=").*(?=See more.)' | sed "s/.*,/$word:/")
+	if [ "$cmd" == "" ] ; then
+		printf "this word wasn't found. \n"
+		exit "1"
+	else
+		printf "$cmd \n"
+	fi
+}
+
+usage() {
+	printf "usage: \n    dict <word> \n    requires curl, grep and sed. \n"
+	exit "1"
+}
+
+# exec
+if [ "$1" == "" ] ; then
+	usage
+else
+	get
+fi
