ba0290a2db5230d6240d7a1836c8a12ee33901a7
eti0 <eti@eti.tf>
2018-04-14 23:36:00 +0200

    updated scripts/toggle-bar.sh

--- scripts/toggle-bar.sh
+++ scripts/toggle-bar.sh
@@ -4,11 +4,10 @@
 # vars
 tmpimg="/tmp/toggle.png"
 pid="$(pidof "lemonbar")"
-pid2="$(pidof "tint2")"
 width="$(xdotool "getdisplaygeometry" | awk '{print $1;}')"
 height="$(xdotool "getdisplaygeometry" | awk '{print $2;}')"
 y="0"
-h="60"
+h="30"
 
 # import colors
 source "/usr/scripts/colors.sh"
@@ -19,18 +18,16 @@ if [ $pid ] ; then
 	openbox --reconfigure
 
 	kill -9 "$pid"
-	kill -9 "$pid2"
 
 	sleep ".5s" && kill -9 "$(pidof n30f)" &
 else
-	convert -size "$width\ x\ $h" "xc:$background" "$tmpimg"
+	convert -size "$width x $h" "xc:$background" "$tmpimg"
 	n30f "$tmpimg" -y "$y" &
 
 	sed -i "s/<top>0<\/top>/<top>$h<\/top>/g" "$HOME/.config/openbox/rc.xml"
 	openbox --reconfigure
 
 	sleep ".5s" && wbr &
-	sleep ".5s" && tint2 -c "$HOME/.config/tint2/slim" &
 
-	sleep "1.5s" && kill -9 "$(pidof n30f)" &
+	sleep "1s" && kill -9 "$(pidof n30f)" &
 fi
