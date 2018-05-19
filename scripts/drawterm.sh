d1f6921fe28a37892f57386f251980f307d06111
eti0 <eti@eti.tf>
2018-05-19 17:37:28 +0200

    added scripts/drawterm.sh

---
+++ scripts/drawterm.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# requires urxvt, slop, xdotool and wmctrl.
+
+
+# vars
+border="10"
+color="1 1 1 0"
+format="%w %h %x %y"
+
+
+# exec
+urxvt -iconic -name "drawterm" &
+read "width" "height" "xpos" "ypos" < <(slop -l -f "$format" -b "$border" -c "$color" )
+
+# adapt to the border width of your windows
+# ((width -= 30))
+((height -= 42))
+
+active="$(xdotool search --sync --classname drawterm)"
+
+xdotool "windowmove" "$active" "$xpos" "$ypos"
+xdotool "windowsize" "$active" "$width" "$height"
+wmctrl -ia "$active"
