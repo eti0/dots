c7e3118e4583ce5ef78d28c45a2f09edc4552b5a
eti0 <eti@eti.tf>
2018-05-22 16:03:51 +0200

    updated scripts/wbr.sh

--- scripts/wbr.sh
+++ scripts/wbr.sh
@@ -104,7 +104,7 @@ laptop_loop() {
 	while :; do
 		echo "%{l}\
 		$padding$(desktop)$padding\
-		%{A:mpc 'toggle' &:}%{A2:cover &:}%{A3:urxvt -e 'ncmpcpp' &:}$(mpd)%{A}%{A}%{A}%{A:sps 'play' &:}$(spotify)$padding%{A}\
+		%{A:mpc 'toggle' &:}%{A2:cover &:}%{A3:urxvt -e 'ncmpcpp' &:}$(mpd)%{A}%{A}%{A}%{A:sps 'play' &:}%{A2:notify-send 'looking for a cover' && cover-spotify &:}$(spotify)$padding%{A}%{A}\
 		%{r}\
 		%{A:batstat &:}$padding$(battery)$padding%{A}\
 		-\
