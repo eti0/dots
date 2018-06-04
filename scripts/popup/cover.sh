28be33f0fa0c4d5492130c4c18c24e94982a65ed
eti0 <eti@eti.tf>
2018-06-04 18:44:15 +0200

    u scripts/popup/cover.sh

--- scripts/popup/cover.sh
+++ scripts/popup/cover.sh
@@ -2,6 +2,9 @@
 
 
 # vars
+set dir "$HOME/.covers"
+set file "$dir/cover.png"
+
 if test (pidof spotify)
     set title (sps current | sed "s/.* - //")
     set artist (sps current | sed "s/ - .*//")
@@ -12,16 +15,15 @@ else
     set album (mpc current -f "%album%")
 end
 
-set file "/tmp/cover.png"
 
 # exec
-if test -f "/tmp/$artist - $album.png"
-    set file "/tmp/$artist - $album.png"
+if test -f "$dir/$artist - $album.png"
+    set file "$dir/$artist - $album.png"
 else
     notify-send "looking for a cover"
     glyrc cover --album "$album" --artist "$artist" --title "$title" --write "$file"
     convert "$file" -resize "200x200" "$file" >/dev/null 2>&1
-    cp "$file" "/tmp/$artist - $album.png"
+    cp "$file" "$dir/$artist - $album.png"
 end
 
 if test -f "$file"
