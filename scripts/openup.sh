bf7458e8b5011dd7b3264bad9cd252e8a4994071
eti0 <eti@eti.tf>
2018-09-18 21:54:43 +0200

    a scripts/openup.sh ; u config/dunstrc & x/tch.xres

---
+++ scripts/openup.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env fish
+# a small program to determinate which tool should be used to open a link
+
+
+# vars
+set url $argv
+set tmp "/tmp/file"
+set short (curl -sLI $url | grep 'Location' | sed 's/Location: //')
+
+
+# funcs
+function short
+    if test -z $short
+        echo "url isn't shortened"
+    else
+        echo "shortened url leading to: $short"
+        set -x url $short
+    end
+end
+
+function download
+    wget $url -O $tmp
+end
+
+function video
+    urxvt -g 100x10 -e mpv $tmp
+end
+
+function audio
+    urxvt -g 100x10 -e mpv $tmp
+end
+
+function text
+    subl $url
+end
+
+function pic
+    sxiv -ab $tmp
+end
+
+function other
+    chromium $url
+end
+
+
+# exec
+short
+
+# laziness
+set ext (echo $url | sed 's/^.*\.//;s/?.*//')
+
+switch $ext
+    case ""
+        echo "i require an argument"
+        exit 1
+    case png jpg jpeg gif tif
+        other
+    case aac mp3
+        audio
+    case mp4 avi mkv
+        video
+    case *
+        other
+        exit 1
+end
