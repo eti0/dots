e9562919f607a4493fa930c577703b031d4272cf
eti0 <eti@eti.tf>
2019-03-11 21:26:47 +0100

    2m2t :o

--- scripts/drawterm.sh
+++ scripts/drawterm.sh
@@ -1,30 +1,31 @@
 #!/usr/bin/env bash
-# requires urxvt, slop, xdotool and wmctrl.
+# requires urxvt, slop & xdotool
+
 
 # vars
-border="2"
-color="0 0 0 1"
-format="%w %h %x %y"
-dir="/usr/scripts"
+borderWidth="0"
+selectionFormat="%w %h %x %y"
+selectionColors="0 0 0 1"
+selectionBorderWidth="2"
 
 
 # exec
 urxvt -iconic -name "drawterm" &
-read "width" "height" "xpos" "ypos" < <(slop -t "0" -l -f "$format" -b "$border" -c "$color")
+read "width" "height" "xpos" "ypos" < <(slop -t "0" -lf "$selectionFormat" -b "$selectionBorderWidth" -c "$selectionColors")
 
 if [ "$width" ] ; then
-	:
+    :
 else
-	pkill -n -f "urxvt -iconic -name drawterm"
-	exit "1"
+    pkill -nf "urxvt -iconic -name drawterm"
+    exit "1"
 fi
 
 # adapt to the border width of your windows
-((width -= 0))
-((height -= 0))
+((width -= $borderWidth))
+((height -= $borderWidth))
 
-active="$(xdotool search --sync --classname drawterm | tail -n1)"
+active="$(xdotool 'search' --sync --classname 'drawterm' | tail -n1)"
 
 xdotool "windowmove" "$active" "$xpos" "$ypos"
 xdotool "windowsize" "$active" "$width" "$height"
-wmctrl -ia "$active"
+xdotool "windowactivate" "$active"
